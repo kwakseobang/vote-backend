41994fc616c5e42694c80259a162c5eb7517ab54
kwakseobang <iii6602@naver.com>
2024-12-07 23:17:21 +0900

    Feat[#6]: JPA repository Crate

--- src/main/java/com/kwakmunsu/vote/repository/UserRepository.java
+++ src/main/java/com/kwakmunsu/vote/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.kwakmunsu.vote.repository;
 
 import com.kwakmunsu.vote.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByNickname(String nickname);
 
 }
