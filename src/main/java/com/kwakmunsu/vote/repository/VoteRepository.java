0b629b05f6233ed3825b7e259c5378743d62ac0a
kwakseobang <iii6602@naver.com>
2024-12-07 23:17:21 +0900

    Feat[#6]: JPA repository Crate

--- src/main/java/com/kwakmunsu/vote/repository/VoteRepository.java
+++ src/main/java/com/kwakmunsu/vote/repository/VoteRepository.java
@@ -3,6 +3,6 @@ package com.kwakmunsu.vote.repository;
 import com.kwakmunsu.vote.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoteRepository extends JpaRepository<Long, Vote> {
+public interface VoteRepository extends JpaRepository<Vote,Long> {
 
 }
