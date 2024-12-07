90de3983f4be6a3e1624515175bbfa511af05de7
kwakseobang <iii6602@naver.com>
2024-12-07 14:18:49 +0900

    Chore[#3]: @EnableJpaAuditing 추가

--- src/main/java/com/kwakmunsu/vote/VoteApplication.java
+++ src/main/java/com/kwakmunsu/vote/VoteApplication.java
@@ -2,7 +2,9 @@ package com.kwakmunsu.vote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class VoteApplication {
 
