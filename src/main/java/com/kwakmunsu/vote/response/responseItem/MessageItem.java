4959335c199dcb42ffeb6b8f0ad9f9ce0f676662
kwakseobang <iii6602@naver.com>
2024-12-17 02:57:54 +0900

    Feat[#9]: token message update

--- src/main/java/com/kwakmunsu/vote/response/responseItem/MessageItem.java
+++ src/main/java/com/kwakmunsu/vote/response/responseItem/MessageItem.java
@@ -38,10 +38,17 @@ public class MessageItem {
         public static final String PREVENT_GET_ERROR = "Status 204 - 리소스 및 리다이렉트 GET호출 에러 방지";
 
         // < Token >
+        public static final String TOKEN_IS_VALID = "VALID - 유효한 토큰  ";
         public static final String REISSUE_SUCCESS = "SUCCESS - JWT Access 토큰 재발급 성공";
         public static final String TOKEN_EXPIRED = "ERROR - JWT 토큰 만료 에러";
         public static final String TOKEN_ERROR = "ERROR - 잘못된 JWT 토큰 에러";
         public static final String BAD_REQUEST_TOKEN = "ERROR - 잘못된 토큰 요청 에러";
+
+        public static final String TOKEN_IS_BLACKLIST = "ERROR - 폐기된 토큰";
+        public static final String TOKEN_HASH_NOT_SUPPORTED = "ERROR - 지원하지 않는 형식의 토큰";
+        public static final String WRONG_AUTH_HEADER = "ERROR - [Bearer ]로 시작하는 토큰이 없습니다.";
+        public static final String TOKEN_VALIDATION_TRY_FAILED = "ERROR - 토큰 인증 실패";
+
 
         // < Etc >
         public static final String INTERNAL_SERVER_ERROR = "ERROR - 서버 내부 에러";
