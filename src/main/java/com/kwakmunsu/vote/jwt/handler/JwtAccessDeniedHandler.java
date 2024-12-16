f569eaad23aa6650dafc026fc9e25a960b5064da
kwakseobang <iii6602@naver.com>
2024-12-17 03:04:06 +0900

    Feat[#9]: JWT Exception 구현

---
+++ src/main/java/com/kwakmunsu/vote/jwt/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.kwakmunsu.vote.jwt.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+            AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        // 접근에 필요한 권한이 없이 접근하려 할때는 Forbidden (403)
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
