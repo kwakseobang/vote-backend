86d2e96372ec9edccb1e876c3709e6544abff320
kwakseobang <iii6602@naver.com>
2024-12-17 14:18:40 +0900

    Feat[#9]: refreshTokenResponse 추가

--- src/main/java/com/kwakmunsu/vote/dto/AuthDto.java
+++ src/main/java/com/kwakmunsu/vote/dto/AuthDto.java
@@ -63,6 +63,13 @@ public class AuthDto {
         private String grantType; // Bearer
         private String accessToken;
         private Long accessTokenExpiresIn;
+        //rt는 보안 강화를 위해 쿠키에 담아서 보냄
+    }
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class RefreshTokenResponse {
+        // rt는 쿠키에 담아서 보내는데 발급 후 service단에서 가져오기 위해서 만듦
         private String refreshToken;
     }
 
